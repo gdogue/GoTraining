cec41186a7687be528ccd7fd7832f0eafc729583
gdogue <gwrichardson42@yahoo.com>
2021-03-31 15:21:55 -0700

    added go func

--- code-samples/NinjaL9/157-Exercise4.go
+++ code-samples/NinjaL9/157-Exercise4.go
@@ -2,29 +2,28 @@ package main
 
 import (
 	"fmt"
-	"runtime"
 	"sync"
 )
 
 func main() {
 
 	var wg sync.WaitGroup
-
 	var myLock sync.Mutex
 
 	counter := 0
 	wg.Add(100)
 	for i := 0; i < 100; i++ {
-		myLock.Lock()
-		v := counter
-		runtime.Gosched()
-		v++
-		counter = v
-		myLock.Unlock()
-		wg.Done()
+		go func() {
+			myLock.Lock()
+			v := counter
+			v++
+			counter = v
+			fmt.Println(counter)
+			myLock.Unlock()
+			wg.Done()
+		}()
 	}
 
-	fmt.Println("Before Wait...")
 	wg.Wait()
 	fmt.Println("Done, Counter:", counter)
 }
